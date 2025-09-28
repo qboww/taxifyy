a18e9461c4c97d78cd668fd13ae3a08af6d63807
Yevhenii Sarancha <yevhenii.sarancha@sigma.software>
2025-09-28 20:54:31 +0300

    added speed insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './components/App/App'
 import './index.css'
 
@@ -8,5 +9,6 @@ import "./firebase";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>,
 )
