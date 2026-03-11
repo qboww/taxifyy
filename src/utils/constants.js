a6570e05c631041e928bb47cb4f4baf7930c4126
Yevhenii Sarancha <yevhenii.sarancha@sigma.software>
2026-03-11 16:28:11 +0200

    min salary update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const MIN_SALARY = 8000; // мінімальна ЗП (грн)
+export const MIN_SALARY = 8647; // мінімальна ЗП (грн)
 export const ESV_COEFFICIENT = 0.22; // ставка ЄСВ
 export const SINGLE_TAX_RATE = 0.05; // 5% єдиний податок
 export const WAR_TAX_RATE = 0.01; // 1% військовий збір
